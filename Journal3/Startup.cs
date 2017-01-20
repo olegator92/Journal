862bca40e7018226ddd32b504e672374077c934b
Oleg Shishkin <olegatormvc@gmail.com>
2017-01-20 17:40:44 +0600

    Add project files.

---
+++ Journal3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Journal3.Startup))]
+namespace Journal3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
